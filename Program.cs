4d7790a7adc61e97f9f651aef6f345aa31ea9aae
biven <biven@DESKTOP-C663FDP>
2021-09-21 19:59:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tech_Assignment___3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a KM-Increment value between 10 and 25:");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int value_of_input = int.Parse(input);
+
+                miles = KM * 0.621371;
+
+                const int size = 3;
+                int [] myvalues = new int[size];
+
+                for (int i = 0; i < size; i++) ;
+                {
+                    myvalues [i] = value_of_input * 0.621371;
+                }
+
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a integer value between 10 and 25");
+            }
+        }
+    }
+}
